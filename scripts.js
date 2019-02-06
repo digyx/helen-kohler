a51c4e364798259c2d7b1d580c1f3f55399837f8
digyx <dtingley@twilitpublications.com>
2019-02-06 14:44:42 -0500

    Initial commit.

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var page = new Vue({
+	el: '#page',
+	data: {
+		name: 'Helen Kohler',
+		instrument: 'Pianist',
+		spotify: 'https://open.spotify.com/artist/3mCB4aLvvnxEFuzq3DSVBL',
+		google: 'https://play.google.com/store/music/artist/Helen_Kohler?id=A6ayynzizm7fjtho4cul7g2x46a',
+		youtube: 'https://www.youtube.com/channel/UCOE2n8EXKJnt67mDJ0hmMnw',
+		amazon: 'https://www.amazon.com/s/ref=ntt_srch_drd_B07C6N9TJ7?ie=UTF8&field-keywords=Helen%20Kohler&index=digital-music&search-type=ss',
+		instagram: 'https://www.instagram.com/the.helen.of.troy/',
+		header: 'background-image: url("header.jpg")',
+		body: 'background-image: url("body.jpg")',
+		footer: 'background-image: url("footer.jpg")',
+	},
+});
